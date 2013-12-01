4b242a3bdfe115632b5622104f9de14f7af88497
JohnMalc <cincenko@seznam.cz>
2013-12-01 13:50:10 +0100

    sdf

--- number 5/l.sql
+++ number 5/l.sql
@@ -3,8 +3,10 @@ GO
 -- Geht nicht
 SELECT [Branch ID], city, (case when M/F=1 then 'Avarage' 
 								when F/M > 1 then 'Over Woman' 
-								when M/F > 1 then 'Over Man'  
+								when M/F > 1 then 'Over Man' 
+								--when M/F is null then 'Unter' 
 							end)  as Wah
+							
 FROM Bill_j
 pivot ( Max(Zahl) for Gender in (M,F) ) as s 
 
