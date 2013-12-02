66c58908c2b74e276167714a88c85f5d4953684a
JohnMalc <cincenko@seznam.cz>
2013-12-02 12:53:50 +0100

    as

--- number 5/k.sql
+++ number 5/k.sql
@@ -1,19 +1,27 @@
 USE [StayHomewithoutPer]
 GO
--- Neukaze Boo2 - Pozor na to
 ----View 
 --SELECT        COUNT([EMPl Id]) AS zahl, Gender, [Branch ID]
 --FROM            dbo.Mit
---WHERE        (Position = 'Supervisor')
+--WHERE        (Position = 'Supervisor') 
 --GROUP BY Gender, [Branch ID]
 --UNION
 --SELECT null, null, [Branch ID]
+--FROM Mit
+--WHERE Position != 'Supervisor'
+
+--ODER
+
+--SELECT null, null, [Branch ID]
 --FROM Fil
 --WHERE [Branch ID]= 'B002'
- 
+
+-- http://technet.microsoft.com/en-us/library/ms177410%28v=sql.105%29.aspx
+--PIVOT(<aggregation function>(<column being aggregated>)
+ -- wenn nicht isnull dann wird "NULL" in field sein  
 SELECT [Branch ID], ISNULL(M, 0) as M, ISNULL(F, 0) as F
   FROM Bill_k
-  pivot( Min(zahl) for Gender in (M,F) ) as test
+  pivot( Sum(zahl) for Gender in (M,F) ) as test
 GO
 
 
