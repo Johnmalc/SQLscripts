9de29cda8e9025a9beb7ce0f2876e718e13e9719
JohnMalc <cincenko@seznam.cz>
2013-12-03 17:53:53 +0100

    new

--- number 5/4/l.sql
+++ number 5/4/l.sql
@@ -1,14 +1,29 @@
-USE [StayHomewithoutPer]
+USE [StayHomewithoutPer];
 GO
+
 -- 100% Fertig 
-SELECT [Branch ID], city, (case when M/F=1 then 'Avarage' 
-								when F/M > 1 then 'Over Woman' 
-								when M/F > 1 then 'Over Man' 
-								when M is null then 'Over Woman' 
-								when F is null then 'Over Man'
-							end)  as Wah
-FROM Bill_j
-pivot ( Max(Zahl) for Gender in (M,F) ) as s 
+
+SELECT [Branch ID] , 
+       CASE
+       WHEN M / F = 1 THEN 'Average'
+       WHEN F / M > 1 THEN 'Over Woman'
+       WHEN M / F > 1 THEN 'Over Man'
+       WHEN M IS NULL THEN 'Over Woman'
+       WHEN F IS NULL THEN 'Over Man'
+       END AS Wah
+  FROM( 
+        SELECT [Branch ID] , 
+               MIN( CASE
+                    WHEN Gender = 'M' THEN zahl
+                    END
+                  )AS M , 
+               MIN( CASE
+                    WHEN Gender = 'F' THEN zahl
+                    END
+                  )AS F
+          FROM Bill_j
+          GROUP BY [Branch ID]
+      )AS test;
 GO
 
 
