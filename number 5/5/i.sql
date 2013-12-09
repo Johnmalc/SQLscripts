3632e5b5668574453f362650923cad90339e6a32
JohnMalc <cincenko@seznam.cz>
2013-12-09 14:58:32 +0100

    jkb

---
+++ number 5/5/i.sql
@@ -0,0 +1,27 @@
+USE [StayHomewithoutPer]
+GO
+
+WITH cteLevels
+    AS ( SELECT CAST( REPLICATE( '.' ,1 )+Name AS varchar( 25 ))AS [Categoty Name] ,
+                [EMPl Id] ,
+                [reports to the Boss], Position 
+                --CAST( Id AS varchar( max ))AS [Order] ,
+                --1 AS Level
+           FROM Mit
+		 where [Branch ID] = 'B003'
+         -- where id=1
+ 
+         --UNION ALL
+         --SELECT CAST( REPLICATE( '.' ,1 )+Name AS varchar( 25 ))AS [Categoty Name] ,
+         --       Mit.[EMPl Id] ,
+         --       Mit.[reports to the Boss] 
+         --       --Level+1 AS Level ,
+         --       --[Order]+'.'+CAST( Novatest.Id AS varchar( max ))AS [Order]
+         --  FROM
+         --       Mit INNER JOIN cteLevels
+         --       ON cteLevels.[EMPl Id] = Mit.[reports to the Boss] 
+		)
+    SELECT *
+      FROM cteLevels
+   --   ORDER BY [Order];
+GO
